daeda93ace849097296cb5380dd293b086d27a1e
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-16 05:07:25 +0700

    menampilkan daftar mahasiswa dalam bentuk list yang dapat diklik untuk menampilkan detail mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/prak7/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/prak7/ui/view/mahasiswa/HomeMhsView.kt
@@ -147,3 +147,25 @@ fun BodyHomeMhsView (
         }
     }
 }
+
+@Composable
+fun ListMahasiswa (
+    listMhs: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(
+        modifier = modifier
+
+    ) {
+        items(
+            items = listMhs,
+            itemContent = { mhs ->
+                CardMhs(
+                    mhs = mhs,
+                    onClick = { onClick(mhs.nim) }
+                )
+            }
+        )
+    }
+}
