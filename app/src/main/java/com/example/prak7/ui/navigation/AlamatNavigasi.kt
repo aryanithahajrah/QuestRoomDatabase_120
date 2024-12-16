b8184f1a980a662b256f5b82f1da67f95f3ba807
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2024-12-16 14:43:34 +0700

    mengganti cara penulisan menjadi lebih rapih

--- app/src/main/java/com/example/prak7/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/prak7/ui/navigation/AlamatNavigasi.kt
@@ -3,19 +3,16 @@ package com.example.prak7.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
-
 object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
-
 object DestinasiDetail : AlamatNavigasi {
     override val route = "detail"
     const val NIM = "nim"
-    val routesWithArg = "$route/ {$NIM}"
+    val routesWithArg = "$route/{$NIM}"
 }
-
 object DestinasiUpdate : AlamatNavigasi {
     override val route = "update"
-    const val NIM = "NIM"
-    val routesWithArg = "$route/ {$NIM}"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
 }
